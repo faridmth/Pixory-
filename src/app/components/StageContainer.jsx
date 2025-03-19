4470cc222dd293608f1fe8ce75b14a247eaac354
faridmth <methia.farid2001@gmail.com>
2025-03-19 01:48:03 +0100

    made the stage ressponcive and centred the main rect on resize

--- src/app/components/StageContainer.jsx
+++ src/app/components/StageContainer.jsx
@@ -1,47 +1,30 @@
 "use client";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import { Stage, Layer, Rect } from "react-konva";
+const design_Width = 100;
+const design_Height = 100;
 
-const design_Width = 500;
-const design_Height = 500;
+const StageContainer = ({stageSetter}) => {
+  const stageRef = useRef(null);
 
-const StageContainer = () => {
-  const stageContainerRef = useRef(null);
-  const [stageSize, setStageSize] = useState({ width: 0, height: 0 });
+  useEffect(()=>{
+    if (stageRef.current) {
+      stageSetter(pre=>[...pre,stageRef.current])
+    }
+  },[])
 
-  useEffect(() => {
-    const updateSize = () => {
-      if (stageContainerRef.current) {
-        setStageSize({
-          width: stageContainerRef.current.offsetWidth,
-          height: stageContainerRef.current.offsetHeight, // Get the parent's height
-        });
-      }
-    };
-
-    updateSize();
-    window.addEventListener("resize", updateSize);
-    return () => window.removeEventListener("resize", updateSize);
-  }, []);
-  const scale = Math.min(1,stageSize.width / design_Width, stageSize.height / design_Height);
-  console.log(scale)
   return (
-    <div
-      ref={stageContainerRef}
-      className="w-full h-[650px] flex justify-center items-center overflow-hidden"
-    >
-      <Stage width={stageSize.width} height={stageSize.height}  scaleX={scale} scaleY={scale}>
+      <Stage ref={stageRef}>
         <Layer>
           <Rect
-            x={(stageSize.width - design_Width *scale) / 2}
-            y={(stageSize.height - design_Height*scale) / 2}
             width={design_Width}
             height={design_Height}
             fill={"white"}
+            name="main-rect"
           />
+
         </Layer>
       </Stage>
-    </div>
   );
 };
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,73 @@
+"use client"
+import { useEffect, useRef, useState } from "react";
 import NavBar from "./components/NavBar";
 import SideBar from "./components/SideBar";
 import StageContainer from "./components/StageContainer";
 import ToolBar from "./components/ToolBar";
 
+const design_Width = 100;
+const design_Height = 100;
+
 export default function Home() {
+  const [stages,setStages]=useState([])
+  const stagesContRef = useRef()
+
+    useEffect(() => {
+
+      const resizeObserver = new ResizeObserver(entries => {
+        console.log(entries[0])
+        if (stagesContRef.current) {
+          stages.forEach(stage=>{
+            stage.width(0)
+            stage.height(0)
+          })
+          let width = entries[0].borderBoxSize[0].inlineSize
+          let height = stagesContRef.current.offsetHeight
+          let scale = Math.min(
+            (width*0.8) / (design_Width),
+            (height*0.8) / (design_Height)
+          )  
+          
+          stages.forEach(stage=>{
+            stage.setContainer(stagesContRef.current)
+            stage.width(width)
+            stage.height(height)
+            let mainRect = stage.findOne('.main-rect')
+            stage.scaleX(scale)
+            stage.scaleY(scale)
+            mainRect.x((width/(2*scale))-(design_Width/2))
+            mainRect.y((height/(2*scale))-(design_Height/2))
+            stage.draw()
+
+          })
+        }
+
+      });
+      
+      if (stagesContRef.current) {
+        resizeObserver.observe(stagesContRef.current);
+      }
+      
+      return () => resizeObserver.disconnect();
+    }, [stages]);
   return (
    <div className="w-full">
-      <NavBar/>
-      <div className="flex absolute h-[calc(100%-69px)] w-full">
+      <NavBar/> 
+      <div className="w-full flex absolute h-[calc(100%-69px)] ">
         <SideBar/>
-        <main className=" w-full ">
+        <main className=" w-[calc(100%-95px)] bg-red-50">
           <ToolBar/>
-        <StageContainer/>
+              <div
+                className="w-full stages-container
+                 min-h-[calc(100%-40px)] flex flex-col  bg-red-800"
+                ref={stagesContRef}
+              >          
+                <StageContainer stageSetter={setStages} />         
+                <StageContainer stageSetter={setStages} />         
+                <StageContainer stageSetter={setStages} />         
+        </div>
         </main>
       </div>
    </div>
   );
-}
+}     
